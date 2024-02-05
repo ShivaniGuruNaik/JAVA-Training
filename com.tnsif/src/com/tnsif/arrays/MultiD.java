d6b3f0577ea3685b8c321bcb9c702fb6f3066338
HP <HP@DESKTOP-DV82LVT>
2024-02-05 11:10:19 +0530

    Training

---
+++ com.tnsif/src/com/tnsif/arrays/MultiD.java
@@ -0,0 +1,15 @@
+package com.tnsif.arrays;
+
+public class MultiD {
+	static void printarray(int c[][]) {
+		System.out.println("Arrays:");
+		for (int i=0; i<c.length; i++) {
+			for (int j=0; j<c[i].length; j++) {
+				
+			}
+			
+		}
+			
+	}
+
+}
